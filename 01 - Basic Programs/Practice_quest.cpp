3ee24a0d5911f66cd1ce556dfda098f5c04fe758
VISHVESH BHARDWAJ <101561391+Vishvesh-Bhardwaj@users.noreply.github.com>
2022-08-31 16:59:51 +0530

    Add files via upload

---
+++ 01 - Basic Programs/Practice_quest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    
+    char ch;
+    cin>>ch;
+    
+    if (ch>=65 && ch <= 90){
+        cout<<"Uppercase";
+    }
+    else if (ch>=97 && ch<=122){
+        cout<<"Lowercase";
+    }
+    else {
+        cout<<"Numeric";
+    }
+
+    return 0;
+}
