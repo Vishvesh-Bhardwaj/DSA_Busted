ee4d1f392560f56bd89dea1437c5631e676dde87
VISHVESH BHARDWAJ <101561391+Vishvesh-Bhardwaj@users.noreply.github.com>
2022-09-26 00:19:43 +0530

    Add files via upload

---
+++ 06 - Switch Case/01_Switch_case.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    
+    int num = 2;
+
+    switch (num) {
+
+        case 1 :
+
+        cout << " One" << endl;
+        break;
+
+        case 2:
+
+        cout << "Second" << endl;
+        break;
+
+        default :
+
+        cout<< "This is Default case !" << endl;
+    }
+    
+    return 0;
+}
